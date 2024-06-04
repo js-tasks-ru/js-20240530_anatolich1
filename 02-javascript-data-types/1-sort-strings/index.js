6e4818cd162e8ca15fe46b7928bc8586aa7de8db
Artem <katod1978@yandex.ru>
2024-06-04 20:20:07 +0700

    task 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,28 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
+
 export function sortStrings(arr, param = 'asc') {
 
+    const arrCopy = [...arr];
+
+    if (param === 'asc') {
+        return arrCopy.sort((a, b) => {
+            if (a.localeCompare(b, 'ru-RU', { sensitivity: 'base' }) <= 0) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+    } else if (param === 'desc') {
+        return arrCopy.sort((a, b) => {
+            if (a.localeCompare(b, 'ru-RU', { sensitivity: 'base' }) >= 0) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+    } else {
+        throw new Error('Invalid order');
+    }
 }
