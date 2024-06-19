5f6e4e70c3798fc5a395cddf41f806ae1ddae046
Artem <katod1978@yandex.ru>
2024-06-19 20:16:26 +0700

    task 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,42 @@
 export default class NotificationMessage {
 
+    constructor(message, options) {
+        this.message = message;
+        this.options = options || {};
+        this.element = document.createElement('div');
+        this.duration = this.options.duration || 0;
+        this.type = this.options.type || 'success';
+        this.timer = null;
+        this.render();
+      }
+    
+      render() {
+        this.element.textContent = this.message;
+        this.element.className = `notification ${this.type}`;
+      }
+    
+      show(targetElement) {
+        if (targetElement) {
+          targetElement.appendChild(this.element);
+        } else {
+          document.body.appendChild(this.element);
+        }
+        this.timer = setTimeout(() => {
+          this.remove();
+        }, this.duration);
+      }
+    
+      remove() {
+        if (this.timer) {
+          clearTimeout(this.timer);
+        }
+        if (this.element) {
+          this.element.remove();
+        }
+      }
+    
+      destroy() {
+        this.remove();
+        this.element = null;
+      }
 }
