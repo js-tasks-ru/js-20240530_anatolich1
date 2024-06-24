4716d3cd222e044b726e4812ad7310b71e264ebc
Artem <katod1978@yandex.ru>
2024-06-24 22:25:01 +0700

    task5.2.2

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -52,65 +52,54 @@ export default class SortableTable {
   getTableRows() {
     return this.data.map(item => this.getTableRow(item));
   }
-  
+
   getTableRow(item) {
     const row = document.createElement('div');
     row.classList.add('sortable-table__row');
-  
+
     this.headerConfig.forEach((header) => {
       const cell = document.createElement('div');
       cell.classList.add('sortable-table__cell');
-  
+
       if (header.template) {
         cell.textContent = header.template(item);
       } else {
         cell.textContent = item[header.id];
       }
-  
+
       row.append(cell);
     });
-  
+
     return row;
   }
 
   sort(field, order) {
-    if (typeof this.data[0][field] === 'string') {
-      this.data.sort((a, b) => sortStrings([a[field], b[field]], order));
-    } else {
-      this.data.sort((a, b) => order === 'asc' ? a[field] - b[field] : b[field] - a[field]);
+    const fieldConfig = this.headerConfig.find(config => config.id === field);
+
+    if (!fieldConfig || !fieldConfig.sortable) {
+      return;
     }
+
+    const k = order === 'asc' ? 1 : -1;
+
+    if (fieldConfig.sortType === 'string') {
+      this.data.sort((a, b) => k * a[field].localeCompare(b[field], ['ru', 'en'], { caseFirst: 'upper' }));
+    } else {
+      this.data.sort((a, b) => k * (a[field] - b[field]));
+    }
+
+    this.updateTable();
   }
 
-/**  sort(field, order) {
-    const arrCopy = [...field];
-    if (typeof arrCopy[0] === 'string')
-      return sortStrings (arrCopy,order);
-    return arrCopy.sort;
 
 
-   this.data.sort((a, b) => {
-      if (typeof a[field] === 'string' && typeof b[field] === 'string') {
-        if (order === 'asc') {
-          return a[field].localeCompare(b[field], ['ru', 'en'], { caseFirst: 'upper' });
-        } else {
-          return b[field].localeCompare(a[field], ['ru', 'en'], { caseFirst: 'upper' });
-        }
-      }
-
-    });
-  
-  } 
-}
- sortStrings(arr, param) {
-
-  if (param === 'asc') {
-      return arr.sort((a, b) => a.localeCompare(b, ['ru', 'en'], { caseFirst: 'upper' }));
+  updateTable() {
+    const { body } = this.subElements;
+    body.innerHTML = '';
+    body.append(...this.getTableRows());
   }
 
-  return arr.sort((a, b) => b.localeCompare(a, ['ru', 'en'], { caseFirst: 'upper' }));
-} */
-
-  remove(){
+  remove() {
     if (this.element) {
       this.element.remove();
     }
